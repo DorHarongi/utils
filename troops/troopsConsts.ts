6d9e0f6447bfc3d05b80050b2e7e90fbf7807184
Dor Harongi <dor_ha@optimove.com>
2026-01-30 18:48:29 +0200

    balance: Rebalance mid/high tier troops - clearer progression and identities

--- troops/troopsConsts.ts
+++ troops/troopsConsts.ts
@@ -11,30 +11,30 @@ export const swordFighterMinimumArsenalLevel: number = 1;
 export const swordFighterMaterialsCost: MaterialsCost = {wood: 100, stones: 100, crop: 500 };
 
 
-export const axeFighterAttackingStat: number = 7;
+export const axeFighterAttackingStat: number = 10;
 export const axeFighterDefenceStat: number = 3;
 export const axeFighterMinimumArsenalLevel: number = 2;
-export const axeFighterMaterialsCost: MaterialsCost = {wood: 1500, stones: 1500, crop: 2000 };
+export const axeFighterMaterialsCost: MaterialsCost = {wood: 750, stones: 750, crop: 1000 };
 
-export const archerAttackingStat: number = 20;
-export const archerDefenceStat: number = 1;
+export const archerAttackingStat: number = 25;
+export const archerDefenceStat: number = 2;
 export const archerMinimumArsenalLevel: number = 4;
-export const archerMaterialsCost: MaterialsCost = {wood: 3500, stones: 2500, crop: 5000 };
+export const archerMaterialsCost: MaterialsCost = {wood: 3000, stones: 2000, crop: 4000 };
 
-export const magicianAttackingStat: number = 15;
-export const magicianDefenceStat: number = 10;
+export const magicianAttackingStat: number = 20;
+export const magicianDefenceStat: number = 15;
 export const magicianMinimumArsenalLevel: number = 5;
-export const magicianMaterialsCost: MaterialsCost = {wood: 5000, stones: 5000, crop: 7500 };
+export const magicianMaterialsCost: MaterialsCost = {wood: 4000, stones: 4000, crop: 4000 };
 
-export const horsemenAttackingStat: number = 25;
-export const horsemenDefenceStat: number = 15;
+export const horsemenAttackingStat: number = 35;
+export const horsemenDefenceStat: number = 25;
 export const horsemenMinimumArsenalLevel: number = 6;
-export const horsemenMaterialsCost: MaterialsCost = {wood: 15000, stones: 15000, crop: 15000 };
+export const horsemenMaterialsCost: MaterialsCost = {wood: 12000, stones: 12000, crop: 11000 };
 
-export const catapultsAttackingStat: number = 150;
+export const catapultsAttackingStat: number = 200;
 export const catapultsDefenceStat: number = 1;
 export const catapultsMinimumArsenalLevel: number = 8;
-export const catapultsMaterialsCost: MaterialsCost = {wood: 35000, stones: 35000, crop: 25000 };
+export const catapultsMaterialsCost: MaterialsCost = {wood: 25000, stones: 25000, crop: 25000 };
 
 export const lootingAbilityOfTroops: number = 175;
 
