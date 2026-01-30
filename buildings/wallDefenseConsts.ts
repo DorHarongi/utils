60c8696413b40ffe541e1c43ea478b318ad8e721
Dor Harongi <dor_ha@optimove.com>
2026-01-30 17:43:27 +0200

    balance: Rebalance wall defense - polynomial scaling instead of exponential

--- buildings/wallDefenseConsts.ts
+++ buildings/wallDefenseConsts.ts
@@ -1,1 +1,4 @@
-export const wallDefenseByLevel = [0, 25, 50, 100, 200, 400, 800, 1600, 3200, 6400, 12800];
+// Balanced wall defense - meaningful at all levels but doesn't make attacking impossible
+// Old values doubled each level (2x), making high-level walls impenetrable
+// New values use polynomial scaling: roughly level * 50 + level² * 10
+export const wallDefenseByLevel = [0, 60, 140, 240, 360, 500, 660, 840, 1040, 1260, 1500];
